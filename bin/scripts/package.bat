fb38476b7e3f7f0a2d932cc45042411ea0c50860
sunhaolin <sunhaolin@hotoa.com>
2021-01-19 10:34:09 +0800

    打包及上传脚本

--- bin/scripts/package.bat
+++
@@ -1,4 +0,0 @@
-set VERSION=1.23
-cd ..\..\
-7z a -tzip dist\%VERSION%.zip * -x!.git -x!dist
-pause
---
+++ bin/scripts/package.js
@@ -0,0 +1,28 @@
+require("dotenv-flow").config();
+const { execSync } = require("child_process");
+const VERSION = require("steedos-server/package.json").version;
+const ossFold = process.env.STEEDOS_PKG_OSS_QUICK_INSTALL_FOLD;
+const os = process.env.STEEDOS_PKG_OS;
+if (!ossFold) {
+  console.error("please set env STEEDOS_PKG_OSS_QUICK_INSTALL_FOLD!!!");
+  return;
+}
+if (!os) {
+  console.error("please set env STEEDOS_PKG_OS!!!");
+  return;
+}
+console.log("ossFold: ", ossFold);
+
+let cwd = process.cwd();
+console.log("VERSION: ", VERSION);
+console.log("cwd: ", cwd);
+
+console.log(`${os}: zip start!`);
+execSync(`7z a -tzip dist/steedos-mini-${os}-${VERSION}.zip * -x!.git -x!dist`);
+console.log(`${os}: zip done!`);
+
+console.log(`${os}: upload to aliyun!`);
+execSync(
+  `ossutil --force cp dist/steedos-mini-${os}-${VERSION}.zip ${ossFold}`
+);
+console.log(`${os}: upload done!`);
