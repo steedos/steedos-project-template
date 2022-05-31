2f4f21502418dabba36281a10c1b02ed16b64174
chenzhipei <18605199364@163.com>
2022-06-01 00:31:40 +0800

    调整初始化安装cli工具

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -6,6 +6,3 @@ cp .env .env.local
 
 # git config pull.rebase， Gitpod上从分支更新代码，或者更新其他人提交的代码有时会提示执行 git config pull.rebase，下面使用git的默认配置
 git config --global pull.rebase false
-
-# install steedos-cli
-npm i steedos-cli --global
