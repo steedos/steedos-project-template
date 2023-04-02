ba5b87384e71ceb123f2150358c3452701760a69
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2023-04-02 13:14:46 +0800

    add triggers example

--- services/example-service/package.service.js
+++ services/example-service/package.service.js
@@ -1,3 +1,6 @@
+const objectqlQuerySpaceUsers = require('./api/objectqlQuerySpaceUsers');
+const spaceUsersBeforeUpdate = require('./triggers/spaceUsersBeforeUpdate');
+
 module.exports = {
   name: "example-service",
 
@@ -41,23 +44,7 @@ module.exports = {
         )
       },
     },
-    objectQuerySpaceUsers: {
-      rest: { method: 'GET', path: '/objectql' },
-      async handler(ctx) {
-        return await this.broker.call(
-          'objectql.find', 
-          {
-            objectName: 'space_users',
-            query: {
-              filters: ['user', '=', ctx.meta.user.userId]
-            },
-          },
-          // 如果查询需要带上当前用户的权限，需要传入 user 属性。
-          {
-            user: ctx.meta.user
-          }
-        );
-      }
-    }
+    objectqlQuerySpaceUsers,
+    spaceUsersBeforeUpdate
   }
 }
