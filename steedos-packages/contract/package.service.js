cfd494a6fc3c33f0a60af3050a0eb1f6cae69fb3
lizheng-steedos <lizheng@steedos.com>
2024-09-20 07:59:42 +0000

    合同软件包文件调整

---
+++ steedos-packages/contract/package.service.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: 孙浩林 sunhaolin@steedos.com
+ * @Date: 2023-10-27 12:51:51
+ * @LastEditors: 孙浩林 sunhaolin@steedos.com
+ * @LastEditTime: 2023-10-27 14:58:18
+ * @FilePath: /steedos-ee-gitlab/steedos-packages-oa/workflow/package.service.js
+ * @Description: 
+ */
+
+const service = require("./dist/index.js");
+module.exports = service;
